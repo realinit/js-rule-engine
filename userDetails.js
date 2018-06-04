5721c3148fa57169db976436d8756c572e8a3438
realinit <nitin.1992tyagi@gmail.com>
2018-06-04 18:57:27 +0530

    Update userDetails.js

--- userDetails.js
+++ userDetails.js
@@ -1,57 +1,7 @@
 
 
   const  userDetails = {
-      "creditBureauType": "EXPERIAN",
-      "userData": {
-          "status": "REPORT_RECEIVED",
-          "creditBureauTypeId": "2",
-          "monthlyIncome": "26500",
-          "dob": "1980-01-01 00:00:00",
-          "cityId": "555",
-          "stateId": "16",
-          "employmentTypeId": "1",
-          "occupationTypeId": null,
-          "employerName": "Amazon",
-          "subscription": "1",
-          "bureauTypeId": "2",
-          "dateOfBirth": "1980-01-01 00:00:00",
-          "score": "720",
-          "creditBureau": "EXPERIAN"
-      },
-      accountdetails : [{
-        "subscriberName":"hdfc",
-        "institution":"hdfc",
-        "accountTypeId":"2",
-        "accountStatusId":"72",
-        "accountStatusCodeId":"2",
-        "status":"ACTIVE",
-        "accountHolderType":"Joint",
-        "sanctionAmount":"2600000",
-        "highCredit":"2600000",
-        "creditLimit":null,
-        "creditLimitAmount":null,
-        "currentBalance":"1800000",
-        "amountPastDue":"4999",
-        "rateofInterest":null,
-        "interestRate":null,
-        "valueofCollateral":null,
-        "typeofCollateral":null,
-        "dateReported":"2016-03-31",
-        "dateOpened":"2015-04-30",
-        "lastPaymentDate":"2015-10-02",
-        "openDate":"2015-04-30",
-        "dateClosed":"",
-        "lastPayment":null,
-        "originalTerm":null,
-        "repaymentTenure":"0",
-        "writtenOffAmtTotal":null,
-        "writtenOffAmtPrincipal":null,
-        "settlementAmount":null,
-        "specialComment":"",
-        "emiAmount":"",
-        "cashLimitAmount":"",
-        "paymentHistoryProfile":"210000000??????????????????????????"
-      }]
+      
   };
 
 module.exports = userDetails;
